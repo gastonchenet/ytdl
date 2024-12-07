501cbe7443a08a51ef6dc47ca08c725cb4f8e72d
Gaston Chenet <contact@gastonchenet.fr>
2024-12-07 20:25:50 +0100

    Updated scripts

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -2,14 +2,14 @@
 
 # Initializing variables
 $YTDLRoot = if ($env:YTDL_INSTALL) { $env:YTDL_INSTALL } else { "$Home\.ytdl" }
-$YTDLPath = "$YTDLRoot\ytdl.zip"
+$YTDLPath = "$YTDLRoot\ytdl"
 
 $FileName="ytdl-win-x64"
-$URL = "https://github.com/gastonchenet/ytdl/releases/latest/download/$FileName.zip"
+$URL = "https://github.com/gastonchenet/ytdl/releases/latest/download/$FileName.exe"
 
 # Rebuilding the path to the executable
 Remove-Item $YTDLRoot -Recurse -Force -ErrorAction SilentlyContinue
-New-Item -ItemType Directory -Force -Path "$YTDLRoot\bin" | Out-Null
+New-Item -ItemType Directory -Force -Path "$YTDLRoot" | Out-Null
 
 # Downloading the executable with curl or Invoke-RestMethod
 curl.exe -#SfLo $YTDLPath $URL
@@ -25,48 +25,15 @@ if (!(Test-Path $YTDLPath)) {
   return 1
 }
 
-# Unzipping the executable
-try {
-  Expand-Archive $YTDLPath $YTDLRoot -Force
-} catch {
-  Write-Output "Install Failed - could not unzip $YTDLPath"
-  Write-Error $_
-
-  return 1
-}
-
-# Moving the executable to the bin folder
-Move-Item -Path "$YTDLRoot\$FileName.exe" -Destination "$YTDLRoot\bin\ytdl.exe" -Force
-
-# Cleaning up the zip file
-Remove-Item $YTDLPath -Force
-
 Write-Output "YTDL succesfully installed!"
 
 $CurrentPath = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
 
-if ($CurrentPath -notmatch [RegEx]::Escape("$YTDLRoot\bin")) {
-  $NewPath = "$CurrentPath;$YTDLRoot\bin"
+if ($CurrentPath -notmatch [RegEx]::Escape("$YTDLRoot")) {
+  $NewPath = "$CurrentPath;$YTDLRoot"
   [System.Environment]::SetEnvironmentVariable("Path", $NewPath, [System.EnvironmentVariableTarget]::User)
 } else {
   Write-Warning "`nNote: Another ytdl.exe is already in %PATH% at $($Existing.Source)`nTyping 'ytdl' in your terminal will not use what was just installed."
 }
 
 Write-Output "`nStart another terminal to be able to use the command 'ytdl'"
-
-# Adding the uninstall script
-$rootKey = $null
-
-try {
-  $RegistryKey = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\YTDL"  
-  $rootKey = New-Item -Path $RegistryKey -Force
-
-  New-ItemProperty -Path $RegistryKey -Name "DisplayName" -Value "YTDL" -PropertyType String -Force | Out-Null
-  New-ItemProperty -Path $RegistryKey -Name "InstallLocation" -Value "$YTDLRoot" -PropertyType String -Force | Out-Null
-  New-ItemProperty -Path $RegistryKey -Name "DisplayIcon" -Value "$YTDLRoot\bin\ytdl.exe" -PropertyType String -Force | Out-Null
-  New-ItemProperty -Path $RegistryKey -Name "UninstallString" -Value "powershell -c `"& `'$YTDLRoot\uninstall.ps1`' -PauseOnError`" -ExecutionPolicy Bypass" -PropertyType String -Force | Out-Null
-} catch {
-  if ($null -ne $rootKey) {
-    Remove-Item -Path $RegistryKey -Force
-  }
-}
--- scripts/install.sh
+++ scripts/install.sh
@@ -6,10 +6,10 @@ cd $(dirname $0)/..
 # Initializing variables
 ytdl_root=YTDL_INSTALL
 ytdl_root=${!ytdl_root:-$HOME/.ytdl}
-ytdl_path=$ytdl_root/ytdl.zip
+ytdl_path=$ytdl_root/ytdl
 
 file_name=ytdl-lin-x64
-url=https://github.com/gastonchenet/ytdl/releases/latest/download/$file_name.zip
+url=https://github.com/gastonchenet/ytdl/releases/latest/download/$file_name
 
 # Rebuilding the path to the executable
 if [ -f $ytdl_root ]; then rm -rf $ytdl_root; fi
@@ -20,34 +20,19 @@ mkdir -p $ytdl_root/bin ||
 curl --fail --location --progress-bar -o $ytdl_path $url ||
   error "Failed to download YTDL from \"$url\""
 
-# Extracting the executable from the archive
-command -v unzip > /dev/null ||
-  error "Unzip is not installed. Please install it and try again."
-
-unzip -q $ytdl_path -d $ytdl_root ||
-  error "Failed to extract YTDL to \"$ytdl_root\""
-
-# Renaming the executable
-mv $ytdl_root/$file_name $ytdl_root/bin/ytdl ||
-  error "Failed to rename YTDL executable" 
-
-# Removing the archive
-rm $ytdl_path ||
-  error "Failed to remove YTDL archive"
-
 # Setting permissions on the executable
-chmod +x $ytdl_root/bin/ytdl ||
+chmod +x $ytdl_root/ytdl ||
   error "Failed to set permissions on YTDL executable"
 
 echo "YTDL succesfully installed!"
 
 # Adding the executable to the PATH
-if ! grep -q $ytdl_root/bin <<< $PATH; then
-  export PATH="$PATH:$ytdl_root/bin"
+if ! grep -q $ytdl_root <<< $PATH; then
+  export PATH="$PATH:$ytdl_root"
 
   echo "" >> "$HOME/.bashrc"
   echo "# YTDL" >> "$HOME/.bashrc"
-  echo "export PATH=\"\$PATH:$ytdl_root/bin\"" >> "$HOME/.bashrc"
+  echo "export PATH=\"\$PATH:$ytdl_root\"" >> "$HOME/.bashrc"
 
   echo ""
   echo "Please restart your terminal or run \"source ~/.bashrc\" to be able to use the command 'ytdl'"
